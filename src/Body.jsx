ed694fb5b50abb0221c9b5c4f3ea0d8c275c7791
Aldair André <aldairandre99@gmail.com>
2022-04-26 22:32:32 +0100

    add animacoes de hover

--- src/Body.jsx
+++ src/Body.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import './App-Hover.css'
 
 export default function Main(){
     return(
@@ -12,24 +13,24 @@ export default function Main(){
            <div className="container-info animate__animated animate__fadeInLeftBig animate__delay-1s">
                <div className="txt-info">
                    <h2>Tem uma, tem tudo</h2>
-                   <p>Cerveja angolana reconhecida internacionamente.<br/>Detentora de prémios por ser saborosa e trazer a alegria a quem consome</p>
-                   <button className="quero-btn animate__animated animate__fadeInLeftBig animate__delay-1s">Quero uma</button>
+                   <p className='hvr-grow'>Cerveja angolana reconhecida internacionamente.<br/>Detentora de prémios por ser saborosa e trazer a alegria a quem consome</p>
+                   <button className="quero-btn animate__animated animate__fadeInLeftBig animate__delay-1s hvr-bubble-bottom">Quero uma</button>
                </div>
            </div>
            <div className="luandina-lata-container">
                <img src="./assets/imagens/luandina.png" alt="lata da luandina" className="luandina-lata animate__animated animate__fadeInUpBig animate__delay-2s"/>
                <img src="./assets/imagens/graphic.png" alt="graphic" className="graphic animate__animated animate__fadeInUpBig animate__delay-1s"/>
            </div>
-           <div className="more animate__animated animate__fadeInRightBig animate__delay-1s">
-               <p className="more-txt">Ver mais</p>
-               <i className="bi bi-arrow-right-circle"></i>
+           <div className="more animate__animated animate__fadeInRightBig animate__delay-1s ">
+               <p className="more-txt ">Ver mais</p>
+               <i className="bi bi-arrow-right-circle hvr-grow"></i>
            </div>
            <nav className="follow animate__animated animate__fadeInRightBig animate__delay-1s">
                <ul>
                    <li>Siga-nos</li>
-                   <li><a href="#"><i className="bi bi-facebook"></i></a></li>
-                   <li><a href="#"><i className="bi bi-instagram"></i></a></li>
-                   <li><a href="#"><i className="bi bi-github"></i></a></li>
+                   <li><a href="#"><i className="bi bi-facebook hvr-grow"></i></a></li>
+                   <li><a href="#"><i className="bi bi-instagram hvr-grow"></i></a></li>
+                   <li><a href="#" className='hvr-grow'><i className="bi bi-github "></i></a></li>
                </ul>
            </nav>
        </main>
--- src/Header.jsx
+++ src/Header.jsx
@@ -1,14 +1,20 @@
 import './App.css'
+import './App-Hover.css'
+
+const activeEfect = (id) => {
+  console.log(id);
+}
+
 export default function Header(){
     return(
         <header className='animate__animated animate__fadeInDown animate__delay-1s'>
           <nav>
             <ul className="h-list">
-                <li><a href="#"><img src="./assets/imagens/Logo.png" alt="Logo da Luandina"/></a></li>
-                <li><a className="active" href="#">Home</a></li>
-                <li><a href="#">Lojas</a></li>
-                <li><a href="#">Produção</a></li>
-                <li><a href="#">Sobre</a></li>
+                <li className='hvr-grow'><a href="#"><img src="./assets/imagens/Logo.png" alt="Logo da Luandina" /></a></li>
+                <li><a className="active hvr-underline-from-center" href="#">Home</a></li>
+                <li><a href="#" onClick={activeEfect('item3')} id='item3' className='hvr-underline-from-center'>Lojas</a></li>
+                <li><a href="#" onClick={activeEfect('item4')} id='item4' className='hvr-underline-from-center'>Produção</a></li>
+                <li><a href="#" onClick={activeEfect('item5')} id='item5' className='hvr-underline-from-center'>Sobre</a></li>
             </ul>
           </nav>
           <div className="search">
